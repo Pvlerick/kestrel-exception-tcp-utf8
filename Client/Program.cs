08a4020444444f8321e25fddddf26eb0a2b73d94
Philippe <pvlerick@gmail.com>
2021-03-24 14:55:21 +0100

    Smallest repro I can think of

---
+++ Client/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            Console.WriteLine("Press any key to lauch request");
+            Console.ReadKey();
+
+            const string host = "localhost";
+            const int port = 5000;
+
+            var payload = Encoding.UTF8.GetBytes(
+$@"GET http://{host}:{port}/?ù HTTP/1.1
+Host: {host}:{port}
+Content-Length: 0");
+
+            var client = new TcpClient(host, port);
+            using var stream = client.GetStream();
+
+            await stream.WriteAsync(payload);
+
+            var buffer = new byte[1024];
+            var read = await stream.ReadAsync(buffer);
+        }
+    }
+}
